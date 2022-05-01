89fb05ec98f1a624bcc0def9ee3fdd0d7fa823cf
Minseok-Choi <cms05041@gmail.com>
2022-05-02 00:24:04 +0900

    영속성 컨텍스트에 대한 기본적 이해

--- src/main/java/hellojpa/HelloJpaMain.java
+++ src/main/java/hellojpa/HelloJpaMain.java
@@ -2,13 +2,14 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Map;
 
 public class HelloJpaMain {
 
     public static void main(String[] args) {
         // 어플리케이션 당 하나만 생성되는 것
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-        // 요청마다 생기는 것 쓰레드간 공유X
+        // 요청마다 생기는 것 쓰레드간 공유X 엔티티 매니저와 영속성 컨텍스트가 1:1
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
         EntityTransaction transaction = entityManager.getTransaction();
@@ -16,24 +17,22 @@ public class HelloJpaMain {
 
         try {
 //            Member member = new Member();
-//            member.setId(1L);
+//            member.setId(2L);
 //            member.setName("ron2");
-//            entityManager.persist(member);
+              // 이때 insert sql 쿼리를 날리지 않는다. insert sql은 생성해서 쓰기 지연 sql 저장소에 저장 해둔다. flush
+//            entityManager.persist(member); // 영속성 컨텍스트에 담는다.
 
-//            Member targetMember = entityManager.find(Member.class, 1L);
-//            targetMember.setName("ron3");
+//            Member targetMember = entityManager.find(Member.class, 2L);
+//            Member targetMember1 = entityManager.find(Member.class, 2L);
+//            System.out.println("같은 트랜잭션 안에서 영속 엔티티의 동일성 보장 -> " + (targetMember1 == targetMember));
 
-            // JPQL 엔티티 대상으로 쿼리 객체 지향 SQL
-            List<Member> resultList = entityManager.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(5)
-                    .getResultList();
+            Member targetMember = entityManager.find(Member.class, 2L);
+            targetMember.setName("updated"); // update 퀴리를 보냄 dirty checking
+            entityManager.remove(targetMember); // 삭제
+            entityManager.detach(targetMember); // 영속성 컨텍스트에서 빠진다. -> 준영속 상태(detached)
+            entityManager.clear(); // 영속성 컨텍스트 전체를 초기화
 
-            for (Member member : resultList) {
-                System.out.println("member.name = " + member.getName());
-            }
-
-            transaction.commit();
+            transaction.commit(); // flush 자동 호출
         } catch (Exception e) {
             transaction.rollback();
         } finally {
